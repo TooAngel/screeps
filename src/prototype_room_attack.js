0d2050b34d0d2bc4aba2a93a3c8e463f40e75675
Martin Schultheiss <mschultheiss83@gmail.com>
2017-01-01 18:05:02 +0100

    added Room.prototype.attack42

--- src/prototype_room_attack.js
+++ src/prototype_room_attack.js
@@ -41,6 +41,37 @@ Room.prototype.closestSpawn = function (target) {
   return _.first(shortest).room;
 };
 
+/**
+ * attack42 is called so because 42 is the only true answer :-)
+ *
+ * USAGE: get a Room Object then .attack42('targetRoomName')
+ * e.g. Game.rooms[Memory.myRooms[0]].attack42('targetRoomName');
+ * TODO add an attack42 event if invader is seen by reserver, sourcer or carry
+ *
+ * @param {String} roomName should be your targetRoomName
+ * @param {Array} [spawn] yourCustomSpawn Array of {creeps: creepsToAdd, role: 'rolesToAdd'}
+ */
+Room.prototype.attack42 = function (roomName, spawn) {
+  spawn = spawn || [
+      {creep: 1, role: 'autoattackmelee'},
+      {creep: 1, role: 'defender'},
+      {creep: 1, role: 'squadheal'},
+
+      {creep: 2, role: 'autoattackmelee'},
+      {creep: 2, role: 'defender'},
+      {creep: 2, role: 'squadheal'}
+    ];
+
+  let closestSpawn = this.closestSpawn(roomName);
+  // this.log('closestSpawn ' + JSON.stringify(closestSpawn, null, 2));
+  if (closestSpawn && closestSpawn.id) {
+
+    brain.startMeleeSquad(closestSpawn.room, roomName
+      //, spawn
+    );
+  }
+};
+
 Room.prototype.attackRoom = function () {
   function attack0(room) {
     room.log('Queuing level 0 attack');
