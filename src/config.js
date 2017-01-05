e095b8933f45981674d72191d2fa81f9af45f441
Nevrdid <nevrdid@msn.com>
2017-01-05 20:13:54 +0100

    screepsPlus agent by SemperRabbit
    delete Memory.stats if stats disabled
    
    Not sure actually that this is less expensive than a simpler else{delete
    ...} which will be executed each ticks. If someone know :)
    
    Clarify

--- src/config.js
+++ src/config.js
@@ -36,7 +36,7 @@ global.config = {
     enabled: false,
     summary: false,
     //--- Uncomment screepsplusToken for share datas on TooAngels datasboard on screepspl.us
-    //--- You can acces datas contacting tooangel on tooangels slack channel or use your token.
+    //--- You can acces datas contacting tooangel on #the_angels slack channel or use your token.
     //screepsplusToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRvb2FuZ2VscyIsImlhdCI6MTQ4MzU2MTU3OSwiYXVkIjoic2NyZWVwc3BsLnVzIiwiaXNzIjoic2NyZWVwc3BsLnVzIn0.NhobT7Jg8bOAg-MYqrYsgeMgXEVXGVYG9s3G9Qpfm-o'
   },
 
