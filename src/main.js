b8762f470c9e525618eaf00db3cec90a54b69487
Nevrdid <nevrdid@msn.com>
2017-01-05 20:12:27 +0100

    Add cpu usage in stats

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ require('prototype_creep_move');
 require('prototype_roomPosition');
 require('prototype_room_init');
 require('prototype_room_costmatrix');
+require('screepsplus');
 
 if (config.profiler.enabled) {
   try {
@@ -29,6 +30,9 @@ var main = function() {
 
   Memory.myRooms = _.map(_.filter(Game.rooms, (r) => r.execute()), r => r.name);
 
+  if (config.stats.enabled && Memory.username) {
+    Memory.stats[Memory.username + '.cpu.used'] = Game.cpu.getUsed();
+  }
 };
 
 module.exports.loop = function() {
