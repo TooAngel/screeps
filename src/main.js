f8aada63266762d09f5a6b1002447fe4f5d53c0c
Nevrdid <nevrdid@msn.com>
2017-01-07 02:58:44 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ require('prototype_roomPosition');
 require('prototype_room_init');
 require('prototype_room_costmatrix');
 require('screepsplus');
--require('visualizer');
+require('visualizer');
 
 if (config.profiler.enabled) {
   try {
@@ -37,9 +37,9 @@ var main = function() {
   Memory.myRooms.forEach(function(roomName) {
     brain.stats.addRoom(roomName);
   });
-- if (config.visualizer.enabled && config.visualizer.refresh) {
--   visualizer.render();
-- }
+  if (config.visualizer.enabled && config.visualizer.refresh) {
+    visualizer.render();
+  }
   brain.stats.add('', '.cpu.used', Game.cpu.getUsed());
 };
 
