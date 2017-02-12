4ecc8039da1dcc06bac7b3187c0eef2ffe2d4e01
gsc89 <Change.1989>
2017-02-12 14:41:16 +0000

    Didn't mean to commit grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,13 @@ try {
 module.exports = function(grunt) {
   var account;
   try {
-    account = require('./account.screeps.com');
+    account = require('account.screeps.com');
   } catch (e) {
     account = {
       email: false,
       password: false
     };
   }
-  console.log(account);
 
   grunt.loadNpmTasks('grunt-screeps');
   grunt.loadNpmTasks('grunt-contrib-jshint');
