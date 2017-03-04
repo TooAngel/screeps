2008e6350212cac6c255397263dc0daf6f9e3ab6
invisioblack <jOHNCLARKASH@GMAIL.COM>
2017-03-04 16:42:25 -0600

    Update prototype_room_my.js
    linting

--- src/prototype_room_my.js
+++ src/prototype_room_my.js
@@ -432,7 +432,7 @@ Room.prototype.executeRoom = function() {
   this.handleTerminal();
   this.handleNukeAttack();
 
-  if(!this.memory.spawnTimer){
+  if(!this.memory.spawnTimer) {
     this.memory.spawnTimer = 0;
   }
   var harvesters = this.find(FIND_MY_CREEPS, {
@@ -443,10 +443,10 @@ Room.prototype.executeRoom = function() {
       return false;
     }
   });
-  if(harvesters.length < 1){
+  if (harvesters.length < 1) {
     this.spawnCheckForCreate();
   }
-  if(this.memory.spawnTimer > 100 ||  this.energyAvailable == this.energyCapacityAvailable || (this.controller.level *250 < this.energyCapacityAvailable && Game.Time % 50 === 0 ) || this.memory.attackTimer > 15){
+  if (this.memory.spawnTimer > 100 ||  this.energyAvailable == this.energyCapacityAvailable || (this.controller.level *250 < this.energyCapacityAvailable && Game.Time % 50 === 0) || this.memory.attackTimer > 15) {
     this.memory.spawnTimer = 0;
     this.spawnCheckForCreate();
   } else {
