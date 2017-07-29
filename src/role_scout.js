b6901ddbb2c9baee93e53a3ce14b68d2dd2d04b8
Tobias Wilken <tooangel@tooangel.de>
2017-07-29 21:02:44 +0200

    Scout fix
    Scout
    
    Fix scout spawn

--- src/role_scout.js
+++ src/role_scout.js
@@ -154,7 +154,7 @@ let breadthFirstSearch = function(creep) {
     return false;
   }
 
-  if (creep.pos.roomName !== creep.memory.last.pos3.roomName) {
+  if (creep.memory.last && creep.memory.last.pos3 && creep.pos.roomName !== creep.memory.last.pos3.roomName) {
     creep.moveTo(25, 25);
     return true;
   }
