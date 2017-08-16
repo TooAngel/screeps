488c9cf1ebe19e5d0f3be3a7f5b8ac8c3f84e0e6
Martin Schultheiss <mschultheiss83@gmail.com>
2017-08-16 11:18:57 +0200

    improve Stats => Grafana #61

--- src/config.js
+++ src/config.js
@@ -46,7 +46,10 @@ global.config = {
     costMatrixMemoryMaxGCL: 15
   },
 
+  // use username `tooangels` and password `tooSecretPassword` at https://screepspl.us/grafana
   stats: {
+    screepsPlusEnabled: false,
+    screepsPlusToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRvb2FuZ2VscyIsImlhdCI6MTQ4MzU2MTU3OSwiYXVkIjoic2NyZWVwc3BsLnVzIiwiaXNzIjoic2NyZWVwc3BsLnVzIn0.NhobT7Jg8bOAg-MYqrYsgeMgXEVXGVYG9s3G9Qpfm-o',
     enabled: true,
     summary: false
   },
