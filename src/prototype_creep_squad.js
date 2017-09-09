49d243de3ce9929dda0cb946d4416b2f67a9726f
Tobias Wilken <tooangel@tooangel.de>
2017-09-09 22:16:37 +0200

    Improve fighting
     - Defend reserved rooms
     - Autotrigger all attacks: autoattack, squad.start and attack42
    
    Fixes #35

---
+++ src/prototype_creep_squad.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Creep.prototype.squadMove = function(squad, maxRange, moveRandom, role) {
+  if (this.room.name === squad.moveTarget) {
+    const nextExits = this.room.find(this.memory.routing.route[this.memory.routing.routePos].exit);
+    const nextExit = nextExits[Math.floor(nextExits.length / 2)];
+    const range = this.pos.getRangeTo(nextExit.x, nextExit.y);
+    if (range < maxRange) {
+      Memory.squads[this.memory.squad][role][this.id].waiting = true;
+      if (moveRandom) {
+        this.moveRandom();
+      }
+      return true;
+    }
+  }
+  return false;
+};
