d992298bb9d049edc3c143ed18339cd61daf2262
Martin Schultheiss <mschultheiss83@gmail.com>
2017-09-14 13:45:54 +0200

    removed unused getFriends function

--- src/prototype_room_fight.js
+++ src/prototype_room_fight.js
@@ -1,11 +1,3 @@
-Room.prototype.getFriends = function() {
-  return this.find(FIND_HOSTILE_CREEPS, {
-    filter: function(object) {
-      return brain.isFriend(object.owner.username);
-    },
-  });
-};
-
 Room.prototype.getEnemys = function() {
   return this.find(FIND_HOSTILE_CREEPS, {
     filter: function(object) {
