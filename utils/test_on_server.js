3482bb007d024d3be318e6e5348d62305972c647
Tobias Wilken <tooangel@tooangel.de>
2018-06-13 12:17:44 +0200

    Improve integration tests
     - Use mongodb as screeps backend for tests
     - Use screepsmod-tickrate for test server
     - Remove `creepsNames` from test script, it is not used
     - Extend the players to test
    
    Fixes #101

--- utils/test_on_server.js
+++ utils/test_on_server.js
@@ -9,7 +9,10 @@ const port = 21025;
 
 const players = {
   'W1N7': {x: 43, y: 35},
-  'W8N8': {x: 12, y: 23}
+  'W8N8': {x: 21, y: 28},
+  'W8N1': {x: 33, y: 13},
+  'W5N1': {x: 10, y: 9},
+  'W8N3': {x: 14, y: 17},
 };
 const rooms = Object.keys(players);
 const duration = 600;
@@ -26,11 +29,14 @@ for (let room of rooms) {
   status[room] = {
     controller: null,
     creeps: 0,
-    creepsNames: [],
+    // creepsNames: [],
     progress: 0,
     level: 0,
   };
 }
+
+let mongoPrepared = false;
+
 /**
  * sleep method
  *
@@ -50,7 +56,7 @@ function sleep(seconds) {
  */
 function checkForStatus() {
   for (let key in status) {
-    if (process.argv.length != 2 || status[key].progress === 0 || status[key].level < 2) {
+    if (process.argv.length != 2 || status[key].progress === 0 || status[key].level < 3) {
       console.log(`${key} has no progress ${JSON.stringify(status[key])}`);
       return false;
     };
@@ -138,8 +144,8 @@ const helpers = {
       if (verbose) {
         console.log(event.data.gameTime, 'creeps', JSON.stringify(_.omit(creeps, ['meta', '$loki'])));
       }
-      status[event.id].creepsNames.push(_.map(creeps, 'name'));
-      status[event.id].creepsNames = _.uniq(_.flatten(status[event.id].creepsNames));
+      // status[event.id].creepsNames.push(_.map(creeps, 'name'));
+      // status[event.id].creepsNames = _.uniq(_.flatten(status[event.id].creepsNames));
       status[event.id].creeps += _.size(creeps);
     }
   },
@@ -222,11 +228,19 @@ async function followLog() {
  * @param {object} socket
  * @return {boolean}
  */
-const spawnBots = function(line, socket) {
+const spawnBots = async function(line, socket) {
   if (line.startsWith(`Screeps server v`)) {
+    console.log(`> system.resetAllData()`);
+    socket.write(`system.resetAllData()\r\n`);
+    await sleep(5);
+    const tickDuration = 100;
+    console.log(`> setTickRate(${tickDuration})`);
+    socket.write(`setTickRate(${tickDuration})\r\n`);
+    mongoPrepared = true;
     for (let room of rooms) {
-      console.log('Spawn bot ' + room + ' as TooAngel');
+      console.log('> Spawn bot ' + room + ' as TooAngel');
       socket.write(`bots.spawn('screeps-bot-tooangel', '${room}', {username: '${room}', cpu: 100, gcl: 1, x: ${players[room].x}, y: ${players[room].y}})\r\n`);
+      await sleep(1);
     }
     return true;
   }
@@ -243,7 +257,7 @@ const spawnBots = function(line, socket) {
 const setPassword = function(line, socket) {
   for (let room of rooms) {
     if (line.startsWith(`'User ${room} with bot AI "screeps-bot-tooangel" spawned in ${room}'`)) {
-      console.log(`Set password for ${room}`);
+      console.log(`> Set password for ${room}`);
       socket.write(`storage.db.users.update({username: '${room}'}, {$set: {password: '70dbaf0462458b31ff9b3d184d06824d1de01f6ad59cae7b5b9c01a8b530875ac502c46985b63f0c147cf59936ac1be302edc532abc38236ab59efecb3ec7f64fad7e4544c1c5a5294a8f6f45204deeb009a31dd6e81e879cfb3b7e63f3d937f412734b1a3fa7bc04bf3634d6bc6503bb0068c3f6b44f3a84b5fa421690a7399799e3be95278381ae2ac158c27f31eef99db1f21e75d285802cda983cd8a73a8a85d03ba45dcc7eb2b2ada362887df10bf74cdcca47f911147fd0946fb5119c888f048000044072dcc29b1c428b40b805cadeee7b3afc1e9d9d546c2a878ff8df9fcf805a28cc8b6e4b78051f0adb33642f1097bf0a189f388860302df6173b8e7955a35b278655df2d7615b54da6c63dc501c7914d726bea325c2225f343dff0068ac42300661664ee5611eb623e1efa379f571d46ba6a0e13a9e3e9c5bb7a772b685258f768216a830c5e9af3685898d98a9935cca2ba5efb5e1e4a9f2745c53bff318bda3e376bcd06b06d87a55045a76a1982f6e3b9fb77d39c2ff5c09c76989d1c779655bc2acdf55879b68f6155d14c26bdca3af5c7fd6de9926dbc091da280e6f7e3d727fa68c89aa8ac25b5e50bd14bf2dbcd452975710ef4b8d61a81c8f6ef2d5584eacfcb1ab4202860320f03313d23076a3b3e085af5f0a9e010ddb0ad5af57ed0db459db0d29aa2bcbcd64588d4c54d0c5265bf82f31349d9456', salt: '7eeb813417828682419582da8f997dea3e848ce8293e68b2dbb2f334b1f8949f'}})\r\n`);
       return true;
     }
@@ -258,8 +272,8 @@ const setPassword = function(line, socket) {
  */
 async function checkForSucces(line, defer) {
   let attempts = 0;
-  if (line.startsWith(`'OK'`)) {
-    console.log('Start the simulation');
+  if (mongoPrepared && line.startsWith(`'OK'`)) {
+    console.log('> Start the simulation');
     for (attempts; attempts <= maxAttempts; attempts++) {
       await sleep(duration);
       if (checkForStatus()) {
@@ -292,18 +306,25 @@ async function connectToCli() {
   socket.on('data', async (data) => {
     data = data.toString('utf8');
     const line = data.replace(/^< /, '').replace(/\n< /, '');
-    if (spawnBots(line, socket)) {
+    if (await spawnBots(line, socket)) {
       return;
     }
     if (setPassword(line, socket)) {
       return;
     }
 
-    if (line.startsWith('{ modified: 1 }')) {
+    let parsed;
+    try {
+      parsed = line.substring(26, 60);
+    } catch (e) {
+      console.log(e, line);
+    }
+    if (parsed.indexOf('ok: 1') > -1) {
       ready++;
       if (ready >= rooms.length) {
-        console.log('Listen to the log');
+        console.log('> Listen to the log');
         followLog();
+        console.log(`> system.resumeSimulation()`);
         socket.write(`system.resumeSimulation()\r\n`);
       }
       return;
