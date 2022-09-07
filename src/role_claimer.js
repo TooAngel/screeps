07fa194918aa71601a4ab54d1170c2b76b442d51
XenoAmess <xenoamess@gmail.com>
2022-09-08 05:47:33 +0800

    after claiming, turn claimer to a reserver or a scout, instead of kill it.

--- src/role_claimer.js
+++ src/role_claimer.js
@@ -15,14 +15,21 @@ roles.claimer.settings = {
 };
 
 roles.claimer.action = function(creep) {
-  creep.creepLog('New claimer, in room, claiming');
-  // TODO just added the targetId to the creep, I hope it works
-  // const returnCodeMove = creep.moveTo(creep.room.controller.pos);
-  // console.log(`Move returnCode ${returnCodeMove}`);
-  const returnCode = creep.claimController(creep.room.controller);
-  if (returnCode === OK) {
-    creep.creepLog('New claimer, in room, claimed');
-    creep.suicide();
+  let claimerActionCompleted = creep.memory.claimerActionCompleted;
+  if (!claimerActionCompleted) {
+    creep.creepLog('New claimer, in room, claiming');
+    // TODO just added the targetId to the creep, I hope it works
+    // const returnCodeMove = creep.moveTo(creep.room.controller.pos);
+    // console.log(`Move returnCode ${returnCodeMove}`);
+    const returnCode = creep.claimController(creep.room.controller);
+    if (returnCode === OK) {
+      creep.creepLog('New claimer, in room, claimed');
+      creep.memory.claimerActionCompleted = claimerActionCompleted = true;
+    }
+  }
+  if (claimerActionCompleted) {
+    creep.memory.role = 'scout';
+    return roles.scout.action(creep);
   }
   return true;
 };
