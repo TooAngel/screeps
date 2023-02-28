86c0ecc86ae044e90ac6e91f99d743b1a23c152a
XenoAmess <xenoamess@gmail.com>
2023-02-28 10:07:50 +0800

    bugfix: bugfix for autoattackmelee in a room without controller, and try to attack STRUCTURE_KEEPER_LAIR (#699)

--- src/role_autoattackmelee.js
+++ src/role_autoattackmelee.js
@@ -36,7 +36,7 @@ roles.autoattackmelee.action = function(creep) {
     return true;
   }
 
-  if (creep.room.controller.safeMode) {
+  if (creep.room.controller && creep.room.controller.safeMode) {
     const constructionSites = creep.room.findConstructionSites();
     creep.moveTo(constructionSites[0]);
     return true;
@@ -47,7 +47,7 @@ roles.autoattackmelee.action = function(creep) {
   if (spawn === null) {
     const hostileCreep = creep.findClosestEnemy();
     if (hostileCreep === null) {
-      const structures = creep.pos.findClosestByRangePropertyFilter(FIND_HOSTILE_STRUCTURES, 'structureType', [STRUCTURE_CONTROLLER], {inverse: true});
+      const structures = creep.pos.findClosestByRangePropertyFilter(FIND_HOSTILE_STRUCTURES, 'structureType', [STRUCTURE_ROAD, STRUCTURE_CONTROLLER, STRUCTURE_KEEPER_LAIR, STRUCTURE_WALL], {inverse: true});
 
       if (structures === null) {
         const constructionSites = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
